dc9fdfc28d299db99a36a49b1aa31ee856e13d57
Litmus <davidgatie69@gmail.com>
2026-05-29 16:52:37 +0300

    Refactored Deployment issues and ALLOWED CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -104,6 +104,7 @@ CORS_ALLOWED_ORIGINS = env_list(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "https://vercel.com/muwanguzi-david-jeremiahs-projects/iles-d4ub/9Q4nDFyuxsJiyKyA9bGatUQ9Rsbv",
+        "https://iles-d4ub.vercel.app/",
     ],
 )
 
