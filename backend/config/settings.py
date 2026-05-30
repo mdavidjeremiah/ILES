097f861a03e9b267bf180e7e5fdafcdcba6e5349
Litmus <davidgatie69@gmail.com>
2026-05-30 15:29:23 +0300

    Added render to CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -107,9 +107,10 @@ CORS_ALLOWED_ORIGINS = env_list(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
-        "https://vercel.com",
         "https://iles-sepia.vercel.app",
-        "https://iles-d4ub.vercel.app",    ],
+        "https://iles-d4ub.vercel.app",    
+        "https://iles-1.onrender.com",
+    ],
 )
 
 REST_FRAMEWORK = {
